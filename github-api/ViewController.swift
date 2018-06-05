fdc385194feef65245ee23f4bd2f355fc5310a20
MaxymKrutykh <maxtdty@gmail.com>
2018-06-05 11:38:22 +0300

    Initial Commit

---
+++ github-api/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-api
+//
+//  Created by Maxym Krutykh on 6/5/18.
+//  Copyright © 2018 Maxym Krutykh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
